a50a66361c43807f8e89cf27f4ec42181674d510
Ana Lourenço <a.f.lourenco@icloud.com>
2021-08-04 09:40:24 +0100

    Update item.guard.ts
    wip - added comments guards

--- src/app/shared/guards/item.guard.ts
+++ src/app/shared/guards/item.guard.ts
@@ -10,6 +10,15 @@ export class ItemGuard implements CanActivate {
   constructor(private todoService: TodoService, private router: Router) {
   }
 
+  /**
+  retorna observable --> assincrono
+  promise -> assincrono 
+  boolean -> sincrono
+  
+  Promise - padrao de javascript; 
+  é cncelavel
+  Observable - "superset" de javascropt
+  */
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     // guard é o sitio certo para averiguar se estou a passar um id NUMBER ou outra coisa qq
@@ -21,4 +30,33 @@ export class ItemGuard implements CanActivate {
     }
     return !!item;
   }
+  
+  /*
+  õutros exemplo de guard sao candeactivate e o resolve
+  */
+  
+  //CAN DEACTIVATE
+  
+  
+  
+  
+  /* 
+interface CanDeactivate<T> {
+  canDeactivate(
+  component: T, --> de onde está a tentar sair
+  currentRoute: ActivatedRouteSnapshot, 
+  currentState: RouterStateSnapshot, 
+  nextState?: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
+}
+*/
+
+valida se posso sair da rota e ir apra outra rota
+  casos em que nao funciona sao
+    alteraces diretas no browser ou qq  alteracao que faça sair de angular
+  
+  
+  
+  
+  
 }
